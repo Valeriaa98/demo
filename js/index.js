50ddef2571d91a9e5394bb0adfda4be13bf6ece2
Valeriaa98 <valery.arre090@hotmail.com>
2021-09-26 18:30:06 -0500

    añadido boton para cambair color

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
+const buttonColor = document.querySelector('.change-color-button');
 const button = document.querySelector('.button');
+
+
+buttonColor.addEventListener('click', function() {
+  document.querySelectorAll('p').forEach(
+    el => el.style.color ===  'black' ? el.style.color =  'blue' :el.style.color =  'black'
+);
+});
 
 button.addEventListener('click', function() {
   alert('Hola bienvenido a el mundo minimalista');
